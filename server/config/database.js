8209fa6cc268b669956588733d79183b3d3b4448
Jesse Koivukoski <jesse.koivukoski@taitounited.fi>
2016-10-10 17:38:51 +0300

    Improve secret

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret': 'salasana',
+  'secret': 'e5fb5139b3ebb3a33206fdb4db43d2e5e22b790d75d3c001bb60c1df1c8e3de0',
   'database': 'mongodb://localhost:27017/kurssiarkisto'
 };
