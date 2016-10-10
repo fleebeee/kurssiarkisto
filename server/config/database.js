b87bb99e0771dcd6b004598e17638bab64d7d915
Jesse Koivukoski <jesse.koivukoski@taitounited.fi>
2016-10-10 15:36:07 +0300

    Add Passport authentication and MongoDB

---
+++ server/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'salasana',
+  'database': 'mongodb://localhost:27017/kurssiarkisto'
+};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,144 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var cors = require('cors')
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('./config/database'); // get db config file
+var User = require('./app/models/user'); // get the mongoose model
+var port = process.env.PORT || 3003;
+var jwt = require('jwt-simple');
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(passport.initialize());
+
+mongoose.Promise = require('bluebird');
+mongoose.connect(config.database);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+/*
+db.once('open', function() {
+  var kittySchema = mongoose.Schema({
+    name: String
+  });
+
+  // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+  kittySchema.methods.speak = function () {
+    var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+  }
+
+  var Kitten = mongoose.model('Kitten', kittySchema);
+  var silence = new Kitten({ name: 'Silence' });
+  console.log(silence.name); // 'Silence'
+  var fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.speak(); // "Meow name is fluffy"
+
+  fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+  });
+
+  Kitten.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  });
+
+
+});
+*/
+
+require('./config/passport')(passport);
+var apiRoutes = express.Router();
+
+apiRoutes.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Please pass name and password.'});
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+
+apiRoutes.post('/authenticate', function(req, res) {
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // check if password matches
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          var token = jwt.encode(user, config.secret);
+          // return the information including token as JSON
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
+});
+
+apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false }), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+    User.findOne({
+      name: decoded.name
+    }, function(err, user) {
+        if (err) throw err;
+
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+        } else {
+          res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+        }
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
+});
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+app.use('/api', apiRoutes);
 
 app.get('/', function (req, res) {
   res.json({ message: 'Hello World!' });
 });
 
-app.listen(3003, function () {
-  console.log('Listening on port 3003!');
+app.listen(port, function () {
+  console.log(`Listening on port ${port}!`);
 });
