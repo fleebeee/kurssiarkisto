1e639bdce883d343d0a8b14102053bc05331f3a0
Jesse Koivukoski <jesse.koivukoski@taitounited.fi>
2016-11-02 13:58:15 +0200

    Add Bootstrap

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import Link from 'next/link';
+import css from 'next/css';
+import Head from 'next/head';
+
+import { Button } from 'react-bootstrap';
+
+const index = css({
+  padding: '5%',
+});
+
+const ul = css({
+  listStyleType: 'none',
+  paddingLeft: 0,
+});
+
+const li = css({
+  marginBottom: '5px',
+  padding: '7px',
+  border: '1px solid black',
+  borderRadius: '5px',
+});
 
 export default () => (
-  <div>
+  <div className={index}>
+    {/* TODO Move this to header */}
+    <Head>
+      <title>Kurssiarkisto</title>
+      {/* eslint-disable max-len */}
+      <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css' />
+      <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap-theme.min.css' />
+      {/* eslint-enable max-len */}
+    </Head>
     <h1>Kurssiarkisto</h1>
-    <div>
-      <Link href='/login'><a>Kirjaudu sisään</a></Link>
-    </div>
-    <div>
-      <Link href='/signup'><a>Rekisteröidy</a></Link>
-    </div>
-    <div>
-      <Link href='/preferences'><a>Asetukset</a></Link>
-    </div>
+    <ul {...ul}>
+      <li {...li}>
+        <Link href='/login'>
+          <Button bsStyle='primary'>
+            Kirjaudu sisään
+          </Button>
+        </Link>
+      </li>
+      <li {...li}>
+        <Link href='/signup'><a>Rekisteröidy</a></Link>
+      </li>
+      <li {...li}>
+        <Link href='/preferences'><a>Asetukset</a></Link>
+      </li>
+    </ul>
   </div>
 );
