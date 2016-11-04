d0b06857bce4af26e96bb62668a9470f6eca9936
Ruikka Heta <heta.ruikka@aalto.fi>
2016-11-04 16:53:24 +0200

    Course page cont

--- client/pages/course.js
+++ client/pages/course.js
@@ -10,16 +10,36 @@ const propTypes = {
   url: PropTypes.object,
 };
 
-const Image = styled.img`
-  width: 60px;
-  height: 60px;
+const NameContainer = styled.div`
+  display: flex;
 `;
 
-// Replace this with your own style
+const Arrow = styled.img`
+  width: 55px;
+  height: 55px;
+  margin-right: 15px;
+`;
+
 const CourseContainer = styled.div`
-  display: block;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
 `;
 
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  font-family: 'Helvetica Neue', Helvetica, sans serif;
+  font-size: 1.5em;
+`;
+
+const MSGContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  background-color: grey;
+`;
 
 
 class Index extends Component {
@@ -31,16 +51,24 @@ class Index extends Component {
   render() {
     return (
       <Page>
-        <CourseContainer>
-        <Image
-        src='/static/images/back-arrow.png'
-        alt='Takaisin hakuun'/>
+        <NameContainer>
+          <Arrow
+          src='/static/images/back-arrow.png'
+          alt='Takaisin hakuun'/>
           <h1>CSE-E4400 Design of WWW Services</h1>
-          <p>Tähän sitten niitä kurssin ominaisuuksia ou jee!</p>
+        </NameContainer>
+        <CourseContainer>
+          <InfoContainer>
           <ButtonGroup>
-            <Button bsStyle="warning">Review</Button>
-            <Button bsStyle="warning">Edit</Button>
+            <Button bsStyle="warning" href="/login.js">Arvostele</Button>
+            <Button bsStyle="warning">Muokkaa</Button>
+            <Button bsStyle="warning">Lisää suosikkeihin</Button>
           </ButtonGroup>
+          <p>Tähän sitten niitä kurssin ominaisuuksia ou jee!</p>
+          </InfoContainer>
+          <MSGContainer>
+            <p> tämän pitäisi näkyä oikealla. Tähän alle tulee kommentti boksi </p>
+          </MSGContainer>
         </CourseContainer>
       </Page>
     );
