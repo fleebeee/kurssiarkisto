573d1ea189cf67f5bcaca67d528dba17ed7cde57
Ruikka Heta <heta.ruikka@aalto.fi>
2016-11-05 12:18:19 +0200

    review id

--- server/app/models/review.js
+++ server/app/models/review.js
@@ -2,11 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ReviewSchema = new Schema({
-  reviewID: {
-    type: String, // how is this generated?
-    required: true,
-    unique: true,
-  },
   workload: {
     type: Number, // a number from 0 to 5
     required: true
@@ -16,11 +11,11 @@ var ReviewSchema = new Schema({
     required: true,
   },
   courseID: {
-    type: String, // is this the course code or is a separate ID used?
+    type: String, // courseID
     required: true,
   },
   userID: {
-    type: String, // is this the user email or is a separate ID used?
+    type: String, // userID
     required: true,
   },
 });
