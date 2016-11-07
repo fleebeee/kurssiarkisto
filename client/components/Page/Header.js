4f47d9021c481e48a1be3f077ba47caa76bd4480
Jesse Koivukoski <jesse.koivukoski@taitounited.fi>
2016-11-07 18:49:24 +0200

    Improve LoadingIndicator

--- client/components/Page/Header.js
+++ client/components/Page/Header.js
@@ -2,11 +2,8 @@ import React, { Component, PropTypes } from 'react';
 // import fetch from 'isomorphic-fetch';
 // import ls from 'local-storage';
 import styled from 'styled-components';
-import Head from 'next/head';
 
 import Navigation from './Navigation.js';
-
-global.jQuery = global.$ = require('jquery');
 
 const propTypes = {
   children: PropTypes.node,
@@ -34,18 +31,6 @@ class Header extends Component {
   render() {
     return (
       <HeaderContainer>
-        <Head>
-          <title>Kurssiarkisto</title>
-          <meta charSet='utf-8' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          {/* eslint-disable max-len */}
-          <link rel='stylesheet' href='/static/css/bootstrap.min.css' />
-          <link rel='stylesheet' href='/static/css/ionicons.min.css' />
-          <script src='/static/js/jquery-3.1.1.min.js' />
-          <script src='/static/js/bootstrap.min.js' />
-          {/* eslint-enable max-len */}
-        </Head>
-
         <Logo
           src='/static/images/logo-oranssi.png'
           alt='Kurssiarkisto logo'
--- client/components/Page/Page.js
+++ client/components/Page/Page.js
@@ -2,9 +2,12 @@ import React, { Component, PropTypes } from 'react';
 // import fetch from 'isomorphic-fetch';
 // import ls from 'local-storage';
 import styled from 'styled-components';
+import Head from 'next/head';
 
 import Header from './Header.js';
 import Footer from './Footer.js';
+
+global.jQuery = global.$ = require('jquery');
 
 const propTypes = {
   children: PropTypes.node,
@@ -40,6 +43,18 @@ class Page extends Component {
   render() {
     return (
       <PageContainer>
+        <Head>
+          <title>Kurssiarkisto</title>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          {/* eslint-disable max-len */}
+          <link rel='stylesheet' href='/static/css/bootstrap.min.css' />
+          <link rel='stylesheet' href='/static/css/ionicons.min.css' />
+          <link rel='stylesheet' href='/static/css/loadingindicator.css' />
+          <script src='/static/js/jquery-3.1.1.min.js' />
+          <script src='/static/js/bootstrap.min.js' />
+          {/* eslint-enable max-len */}
+        </Head>
         <HeaderContainer>
           <Header />
         </HeaderContainer>
