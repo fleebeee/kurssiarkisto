f7e8296628404a622a18f5f6cab6cd522fa2884a
Jesse Koivukoski <jesse.koivukoski@taitounited.fi>
2016-12-21 12:55:49 +0200

    Fix link validations

--- server/app/models/course.js
+++ server/app/models/course.js
@@ -27,7 +27,7 @@ const CourseSchema = new Schema({
     validate: {
       validator: (v) => {
         /* eslint-disable max-len */
-        return /^$|^https:\/\/mycourses\.aalto\.fi\/course\/view\.php\?id=[0-9]+$/g.test(v);
+        return /^$|^https:\/\/(www\.)?mycourses\.aalto\.fi\/course\/view\.php\?id=[0-9]+$/g.test(v);
       },
       message: '\'{VALUE}\' is not a valid MyCoursesLink. It should satisfy this RegExp: /^$|^https:\\/\\/mycourses\\.aalto\\.fi\\/course\\/view\\.php\\?id=[0-9]+$/g',
       /* eslint-enable max-len */
@@ -39,7 +39,7 @@ const CourseSchema = new Schema({
     validate: {
       validator: (v) => {
         /* eslint-disable max-len */
-        return /^$|^http:\/\/tenttiarkisto\.fi.*$/g.test(v);
+        return /^$|^http:\/\/(www\.)?tenttiarkisto\.fi.*$/g.test(v);
       },
       message: '\'{VALUE}\' is not a valid TenttiArkisto link. It should satisfy this RegExp: ',
       /* eslint-enable max-len */
